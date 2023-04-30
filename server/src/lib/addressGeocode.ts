1a3bf8f434eb17ba4a96bea0dd36aad7cc4775d9
chepark <chaeahpark.kr@gmail.com>
2023-04-30 23:52:05 +0300

    Added geocode api to get coordinates from address.

--- server/src/lib/addressGeocode.ts
+++ server/src/lib/addressGeocode.ts
@@ -1,3 +1,16 @@
-// https://github.com/pelias/documentation/blob/master/structured-geocoding.md
+// Google Geocode API
+// need api_key from Google Cloud Console
+import axios from "axios";
 
-export const addressGeocode = async () => {};
+type Coordinates = { latitude: number; longitude: number };
+
+export const addressGeocode = async (address: string): Promise<Coordinates> => {
+  const response = await axios.get(
+    `https://geocode.maps.co/search?q=${address}`
+  );
+
+  const { lat }: { lat: string } = response.data[0];
+  const { lon }: { lon: string } = response.data[0];
+
+  return { latitude: parseFloat(lat), longitude: parseFloat(lon) };
+};
