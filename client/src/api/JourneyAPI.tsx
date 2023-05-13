4c9d6bef5a3781140082bb460191c1077a47298b
chepark <chaeahpark.kr@gmail.com>
2023-05-13 18:15:32 +0300

    Modified the return value from getJourneys function.

--- client/src/api/JourneyAPI.tsx
+++ client/src/api/JourneyAPI.tsx
@@ -4,6 +4,9 @@ import { JourneysData } from "../models/journeysInterface";
 const { VITE_BASE_URL } = import.meta.env;
 
 export const getJourneys = async () => {
-  const response: JourneysData = await axios.get(`${VITE_BASE_URL}/journeys`);
-  return response;
+  const response = await axios.get(`${VITE_BASE_URL}/journeys`);
+  const data: JourneysData = response.data;
+  return data.data;
 };
+
+// http://localhost:8000/api/journeys?page=0&sort=journey.id,-distance_meter&departure=Opera&return=Diakoniapuisto&duration=1000-2000&distance=1500-2000
