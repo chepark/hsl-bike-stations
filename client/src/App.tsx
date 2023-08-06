e0e3458316e0b91afcb04cb06dfe1444e354bfec
Chaeah Park <chaeah.park@taiste.com>
2023-08-06 12:07:28 +0300

    Format codes with Prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,23 @@
-import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout/Layout";
-import Stations from "./components/Stations/Stations";
-import Journeys from "./components/Journeys/Journeys";
-import AddJourney from "./components/AddJourney/AddJourney";
-import AddStations from "./components/AddStation/AddStation";
-import AddStation from "./components/AddStation/AddStation";
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import Stations from './components/Stations/Stations';
+import Journeys from './components/Journeys/Journeys';
+import AddJourney from './components/AddJourney/AddJourney';
+import AddStation from './components/AddStation/AddStation';
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route path="stations" element={<Stations />} />
-            <Route path="journeys" element={<Journeys />} />
-            <Route path="jouney/new" element={<AddJourney />} />
-            <Route path="station/new" element={<AddStation />} />
-            <Route path="*" element={<div>404</div>} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="stations" element={<Stations />} />
+          <Route path="journeys" element={<Journeys />} />
+          <Route path="jouney/new" element={<AddJourney />} />
+          <Route path="station/new" element={<AddStation />} />
+          <Route path="*" element={<div>404</div>} />
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { store } from "./app/store.ts";
-import { Provider } from "react-redux";
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { store } from './app/store';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
