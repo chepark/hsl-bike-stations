58ee036796a3a45ba08d52866847b6e62eb1ade3
Chaeah Park <chaeah.park@taiste.com>
2023-08-19 19:43:23 +0300

    Remove previous Table components

--- client/src/components/Journeys/TableFilter.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const TableFilter = () => {
-  return <div>TableFilter</div>;
-};
-
-export default TableFilter;
--- client/src/components/Journeys/TableHeaderCell.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const TableHeaderCell = () => {
-  return <div>TableHeaderCell</div>;
-};
-
-export default TableHeaderCell;
