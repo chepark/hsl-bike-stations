40abecbdc80cf5f30c55e4b4bdc172cbf25097e4
Chaeah Park <chaeah.park@taiste.com>
2023-10-02 16:40:21 +0300

    Fix the bug to fetch journeys on first load

--- client/src/utils/filterUtils.ts
+++ client/src/utils/filterUtils.ts
@@ -1,3 +1,6 @@
-export const stringifiedRange = (range: [number, number]): string => {
+export const stringifiedRange = (
+  range: [number, number]
+): string | undefined => {
+  if (range[0] == 0 && range[1] == 0) return undefined;
   return range.join('-');
 };
