7eaa89cf04245251c4aa5b44f23314c8d5c2d541
Chaeah Park <chaeah.park@taiste.com>
2023-10-15 21:18:59 +0300

    Organize the code

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,13 +1,15 @@
 // sort parameters in journey table
 export const SORT_ORDER = {
-  ASC: "ASC",
-  DESC: "DESC",
+  ASC: 'ASC',
+  DESC: 'DESC',
 };
 
 export const SORT = {
-  BY_ID: "journey.id",
-  BY_STARTING_STATION: "startingStation.name",
-  BY_ENDING_STATION: "endingStation.name",
-  BY_DISTANCE: "route.distance_meter",
-  BY_DURATION: "journey.duration_sec",
+  BY_ID: 'journey.id',
+  BY_STARTING_STATION: 'startingStation.name',
+  BY_ENDING_STATION: 'endingStation.name',
+  BY_DISTANCE: 'route.distance_meter',
+  BY_DURATION: 'journey.duration_sec',
 };
+
+export const GEOCODE_NOMINATIM_BASE_URL = 'https://nominatim.openstreetmap.org';
