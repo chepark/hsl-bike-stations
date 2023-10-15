206e08d2073be7e4c29bb12740350fe17fb17fef
Chaeah Park <chaeah.park@taiste.com>
2023-10-15 20:12:31 +0300

    Edit route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,22 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
-import Stations from './components/Stations';
-import Journeys from './components/Journeys';
-import AddJourney from './components/AddJourney';
-import AddStation from './components/AddStation';
-import StationDetail from './components/StationDetail';
+import Stations from './components/pages/Stations';
+import Journeys from './components/pages/Journeys';
+import AddJourney from './components/pages/AddJourney';
+import AddStation from './components/pages/AddStation';
+import StationDetail from './components/pages/StationDetail';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route path="" element={<Stations />} />
           <Route path="stations" element={<Stations />} />
           <Route path="station/:id" element={<StationDetail />} />
           <Route path="journeys" element={<Journeys />} />
-          <Route path="jouney/new" element={<AddJourney />} />
-          <Route path="station/new" element={<AddStation />} />
+          <Route path="journey/add" element={<AddJourney />} />
+          <Route path="station/add" element={<AddStation />} />
           <Route path="*" element={<div>404</div>} />
         </Route>
       </Routes>
