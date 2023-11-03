97ebda2e7b6bc47b871b94a761fcd771fe4afdc9
Chaeah Park <chaeah.park@taiste.com>
2023-11-03 22:18:23 +0200

    Edit vite config for docker

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   server: {
     host: '0.0.0.0',
     port: 5173,
   },
 });
-=======
-})
->>>>>>> parent of 24277ce (Recover .gitignore)
