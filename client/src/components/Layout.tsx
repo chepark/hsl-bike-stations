e995a2acda074d5f0e9f3621b0a17e1201f7ca50
chepark <chaeahpark.kr@gmail.com>
2023-11-09 11:36:34 +0200

    Fix file imports

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,6 +1,5 @@
-import { Outlet } from 'react-router-dom';
-import Header from './header/Header';
-import { useLocation } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+import Header from './Header/Header';
 
 function Layout() {
   const { pathname } = useLocation();
