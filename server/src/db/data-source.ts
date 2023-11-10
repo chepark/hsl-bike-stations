e6f30de6067d4630a93265c637143b21fbc1af8e
chepark <chaeahpark.kr@gmail.com>
2023-11-10 22:22:04 +0200

    Update Dockerfile and docker-compose to add nginx and dev, prod stages on server

--- server/src/db/data-source.ts
+++ server/src/db/data-source.ts
@@ -8,7 +8,7 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  url: "postgres://postgres:password@docker-postgres-service-name:5432/city-bike", // modify according to docker setup
+  url: "postgres://postgres:Chanchae0331@postgres:5432/city-bike", // modify according to docker setup
   synchronize: true,
   entities: [Station, Route, Journey],
 });
