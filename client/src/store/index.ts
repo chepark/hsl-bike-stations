f7c06772c734afed6466f9e363d042246f2b64aa
chepark <chaeahpark.kr@gmail.com>
2023-11-13 19:24:34 +0200

    Remove redux logger

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,12 +1,9 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
 import journeysReducer from './reducers/journeysSlice';
 import stationReducer from './reducers/stationSlice';
 
 const store = configureStore({
   reducer: { journeys: journeysReducer, stations: stationReducer },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
-  // devTools: process.env.NODE_ENV !== "production",
 });
 
 export type RootState = ReturnType<typeof store.getState>;
