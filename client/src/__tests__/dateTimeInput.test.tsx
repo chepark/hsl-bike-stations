79930b16eaa49f29355f57601c19ea44dcd3efd5
chepark <chaeahpark.kr@gmail.com>
2023-11-18 20:45:06 +0200

    Configure frontend unit testing

---
+++ client/src/__tests__/dateTimeInput.test.tsx
@@ -0,0 +1,37 @@
+import DateTimeInput from '../components/DateTimeInput.js';
+import { MemoryRouter } from 'react-router-dom';
+import { afterAll, beforeAll, describe, expect, it, vi } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/react';
+
+describe('DateTimeInput', () => {
+  const handleChange = vi.fn(); // mock function
+
+  it('should render a label starting with an uppercase.', () => {
+    render(
+      <MemoryRouter>
+        <DateTimeInput
+          field="starting time"
+          handleChange={handleChange}
+          required={true}
+        />
+      </MemoryRouter>
+    );
+
+    const label = screen.getByText(/Starting time/);
+    expect(label).toBeVisible();
+  });
+
+  it('should handle date-time changes', () => {
+    render(
+      <DateTimeInput
+        field="started"
+        handleChange={handleChange}
+        required={true}
+      />
+    );
+
+    const input = screen.getByTestId('dateTimeInput') as HTMLInputElement;
+    fireEvent.change(input, { target: { value: '2021-09-01T12:00' } });
+    expect(input.value).toEqual('2021-09-01T12:00');
+  });
+});
---
+++ client/src/__tests__/setupTest.ts
@@ -0,0 +1,4 @@
+import '@testing-library/jest-dom';
+import '../index.css';
+// import '@testing-library/react';
+// import '@testing-library/user-event';
