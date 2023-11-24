1f73f1ac276eccb78468269d2942d1b4b23e59d9
chepark <chaeahpark.kr@gmail.com>
2023-11-24 22:28:00 +0200

    Clean up unused imports

--- client/src/__tests__/pagination.test.tsx
+++ client/src/__tests__/pagination.test.tsx
@@ -1,6 +1,5 @@
 import { describe, expect, it, vi } from 'vitest';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Pagination from '../components/Pagination';
 import { MemoryRouter } from 'react-router-dom'; // it is needed for useSearchParams
 
