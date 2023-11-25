be6024ae8fb8da7e26354b0bc8ef3bc732b4f0d4
chepark <chaeahpark.kr@gmail.com>
2023-11-25 15:26:11 +0200

    Remove unused variables

--- client/__tests__/table.test.tsx
+++ client/__tests__/table.test.tsx
@@ -1,5 +1,5 @@
 import Table from '../src/components/Table';
-import { describe, expect, it, vi } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
 describe('Table', () => {
